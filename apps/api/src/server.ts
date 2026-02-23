bf1925620dfe8e85bd11967f04d59d14e93198ae
steelptr <68847354+steelptr@users.noreply.github.com>
2026-02-23 22:42:55 +0200

    chore(api): remove PORT from env, use only port 3000

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,17 +4,16 @@ import { migrateToLatest } from "@flsk/database";
 import { db } from "~/lib/db.js";
 
 import { app } from "~/app.js";
-import { env } from "~/lib/env.js";
 
 migrateToLatest(db);
 
 const server = serve({
   fetch: app.fetch,
-  port: env.PORT,
+  port: 3000,
 });
 
 server.on("listening", () => {
-  console.log(`Server is up and listening on port \x1b[33m${env.PORT}\x1b[0m.`);
+  console.log(`Server is up and listening on port \x1b[33m3000\x1b[0m.`);
 });
 
 process.on("SIGINT", () => {
