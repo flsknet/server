9a94d5333512ca5dbd4864f06a0afaa500b55e95
steelptr <68847354+steelptr@users.noreply.github.com>
2026-02-23 22:55:40 +0200

    fix(api): fix crash when cors origin isn't a url

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -14,13 +14,21 @@ const app = new OpenAPIHono();
 app.use(
   cors({
     origin: (origin) => {
-      const url = new URL(origin);
+      try {
+        const url = new URL(origin);
 
-      if (url.hostname == "localhost") {
+        if (
+          url.hostname === "localhost" ||
+          url.hostname === "127.0.0.1" ||
+          url.hostname === "::1"
+        ) {
+          return origin;
+        }
+
+        return env.CORS_ORIGIN;
+      } catch {
         return origin;
       }
-
-      return env.CORS_ORIGIN;
     },
     credentials: true,
   })
